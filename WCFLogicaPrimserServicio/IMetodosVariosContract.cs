1438039d89253fef22c3a361e1633727486883d8
Alejandro López Labajo <mainusone1@gmail.com>
2024-04-03 09:42:51 +0200

    Add project files.

---
+++ WCFLogicaPrimserServicio/IMetodosVariosContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLogicaPrimerServicio
+{
+    [ServiceContract]
+    public interface IMetodosVariosContract
+    {
+        [OperationContract]
+        string GetSaludo(string nombre);
+
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+
+        [OperationContract]
+        List<int> GetTablaMultiplicar(int numero);
+
+        string MetodoInvisible();
+    }
+}
